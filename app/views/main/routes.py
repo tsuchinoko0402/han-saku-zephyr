ca68bfc334fd74503fae22d66853efcffe2b3db2
OKAZAKI Shogo <okazaki@zakioka.net>
2025-03-14 00:36:55 +0900

    ルートの追加

--- app/views/main/routes.py
+++ app/views/main/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app.views.main import main_bp
+from app.models import FileType, Tag, File, ManagerUser
 
 @main_bp.route('/')
 def index():
@@ -10,3 +11,17 @@ def index():
 def about():
     """アバウトページのルート（例）"""
     return render_template('index.html', content="About Page")
+
+@main_bp.route('/api/file-types')
+def file_types():
+    """ファイルタイプの一覧を返すAPI"""
+    types = FileType.query.all()
+    result = [{'id': t.id, 'extension': t.extension, 'description': t.description} for t in types]
+    return jsonify(result)
+
+@main_bp.route('/api/tags')
+def tags():
+    """タグの一覧を返すAPI"""
+    all_tags = Tag.query.all()
+    result = [{'id': t.id, 'name': t.name} for t in all_tags]
+    return jsonify(result)
